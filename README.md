2d9da1fb0f0e2fba5096b4f3ae1f1583d5590e04
unknown <sofikulislameng@gmail.com>
2021-10-19 19:51:24 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome to PatientCare online service
+
+## Available doctor on live support and our PatientCare house.
+## 24 hrs service activated
+## Operation and medicine
+## Special service for eye and pregnancy
+## Free Medicine for poor people
+
---
+++ src/Pages/Home/Bannar/Bannar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Bannar.css'
+
+const Bannar = () => {
+    return (
+        <div className="bannar p-5">
+            <div className="row d-md-flex">
+                <div className="col-md-8 headding">
+                    <h1 className="headding fs-1 text-warning py-5" >We are commited to best service in less money.Hospital services is a term that refers to medical and surgical.
+</h1>
+                </div>
+                <div className="col-md-4"></div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Bannar;
